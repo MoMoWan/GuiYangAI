9762148f6a3627f836ce82150e33c908f2676cda
MoMoWan <859768569@qq.com>
2017-11-04 17:23:04 +0800

    添加主页js,css,html

--- app/models.py
+++ app/models.py
@@ -11,11 +11,11 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from . import app
+from . import app
 from datetime import datetime
 import pymysql
 
-app = Flask(__name__)
+# app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:12345678@127.0.0.1:3306/movie"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
